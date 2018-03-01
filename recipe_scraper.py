13381cd03e6352f65d33588e8d8bcdb7d02073ed
Sarah Ahmad <sarahahmad2019@gmail.com>
2018-03-01 10:38:01 -0600

    added allrecipes scraper and main recipe transformation file

---
+++ recipe_scraper.py
@@ -0,0 +1,54 @@
+import bs4
+import json
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+
+def scrape_ingredients(recipe_url):
+	# url of award show wiki page
+	my_url = recipe_url
+	
+
+	# grab webpage html
+	uClient = uReq(my_url)
+	page_html = uClient.read()
+	uClient.close()
+
+	# html parsing
+	page_soup = soup(page_html, "html.parser")
+
+	recipe_script = []
+	ingredient_list = []
+
+	# find the ingredients by html tag
+	recipe_script = page_soup.findAll('span', {"class": "recipe-ingred_txt added"})
+	for ingredient in recipe_script:
+		text = ingredient.text.strip()
+		ingredient_list.append(text)
+	return ingredient_list
+
+
+def scrape_instructions(recipe_url):
+	my_url = recipe_url
+	
+
+	# grab webpage html
+	uClient = uReq(my_url)
+	page_html = uClient.read()
+	uClient.close()
+
+	# html parsing
+	page_soup = soup(page_html, "html.parser")
+
+	recipe_script = []
+	instructions_list = []
+
+	# find the instructions by html tag
+	recipe_script = page_soup.findAll('span', {"class": "recipe-directions__list--item"})
+	for step in recipe_script:
+		text = step.text.strip()
+		instructions_list.append(text)
+	return instructions_list
+
+
+#print(scrape_instructions('https://www.allrecipes.com/recipe/23600/worlds-best-lasagna/?internalSource=streams&referringId=95&referringContentType=recipe%20hub&clickId=st_recipes_mades'))
---
+++ recipe_transformations.py
@@ -0,0 +1,10 @@
+import recipe_scraper, re
+
+# input recipe url
+url = 'https://www.allrecipes.com/recipe/23600/worlds-best-lasagna/?internalSource=streams&referringId=95&referringContentType=recipe%20hub&clickId=st_recipes_mades'
+
+ingredients_list = recipe_scraper.scrape_ingredients(url)
+instructions_list = recipe_scraper.scrape_instructions(url)
+
+print(ingredients_list)
+print(instructions_list)
