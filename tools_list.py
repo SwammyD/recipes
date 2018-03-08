52156fa5209f0422c00b9c2a46258141eeea1def
SwammyD <samdoty2018@u.northwestern.edu>
2018-03-07 21:56:18 -0600

    tools list file added. Still needs to have cooking methodology implemented.

---
+++ tools_list.py
@@ -0,0 +1,31 @@
+# basic idea: certain cooking terms will be linked to different tools
+# when a cooking term shows up, we know that the corresponding tool is used
+# TODO: attach cooking methodology to each tool.
+tools = [
+
+	"ladle",
+	"fork",
+	"tongs",
+	"butcher's knife",
+	"knife",
+	"oven",
+	"grater",
+	"microwave",
+	"peeler",
+	"blender",
+	"skewer",
+	"brush",
+	"whisk",
+	"spoon",
+	"wooden spoon",
+	"spatula",
+	"grinder",
+	"shaker",
+	"strainer",
+	"slicer",
+	"gloves",
+	"baking sheet",
+	"pan",
+	"pot",
+
+]
